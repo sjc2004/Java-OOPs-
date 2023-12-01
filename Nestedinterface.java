2abace9ad29a8bdf0199d477b892da64607764d0
sjc2004 <118549804+sjc2004@users.noreply.github.com>
2023-12-01 14:11:41 +0530

    Create Nestedinterface.java

---
+++ Nestedinterface.java
@@ -0,0 +1,29 @@
+// Outer class with a nested interface
+class OuterClass {
+    
+    // Nested interface
+    interface NestedInterface {
+        void nestedMethod();
+    }
+}
+
+// Another class implementing the nested interface
+class ImplementingClass implements OuterClass.NestedInterface {
+
+    // Implementing the method from the nested interface
+    @Override
+    public void nestedMethod() {
+        System.out.println("Implemented nested method");
+    }
+}
+
+// Main class to demonstrate usage
+public class Main {
+    public static void main(String[] args) {
+        // Creating an instance of the implementing class
+        ImplementingClass obj = new ImplementingClass();
+
+        // Calling the method from the nested interface
+        obj.nestedMethod();
+    }
+}
